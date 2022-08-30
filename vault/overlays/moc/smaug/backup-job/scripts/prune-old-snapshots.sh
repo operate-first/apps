2c976efc9a247b597b277247fd2596be0e914283
Humair Khan <humair88@hotmail.com>
2022-08-30 13:47:34 -0400

    Move vault prune into separate script.

---
+++ vault/overlays/moc/smaug/backup-job/scripts/prune-old-snapshots.sh
@@ -0,0 +1,13 @@
+PVCRetention=$(cat /backup-job/PVCRetention)
+snapshot_pvc_path=$1
+
+echo "Removing files from PVC that are older than ${PVCRetention} in path ${snapshot_pvc_path}"
+mc rm -r --force --older-than ${PVCRetention} ${snapshot_pvc_path}/
+
+echo "Printing contents of PVC"
+
+echo "--------------------------------------------"
+ls ${snapshot_pvc_path} -lha
+echo "--------------------------------------------"
+
+echo "Done!"
