d6270457edcf1b818c59f140b1f708fc48f8c007
Marcel Hild <hild@b4mad.net>
2022-11-23 08:52:39 +0100

    adjust in case group already exists

--- scripts/onboarding.sh
+++ scripts/onboarding.sh
@@ -5,9 +5,24 @@ set -o allexport -o pipefail -ex
 # - Clone the github.com/operate-first/apps repo at location ${WORKING_DIR}
 # - Working branch should be clean, checked out of upstream default branch.
 # - Set Environment variables: ORG_NAME (=operate-first), SOURCE_REPO (=support), ISSUE_NUMBER
+#
+# If you want to run this script locally, you can set the following environment variables:
+# - create a data.yaml file in the root of the repo
+# - ISSUE_NUMBER=123 ./scripts/onboarding.sh
 
-CONFIG=${PAYLOAD_PATH}
-REPO=${WORKING_DIR}/apps
+# get config from environment or default to data.yaml
+CONFIG=${PAYLOAD_PATH:-data.yaml}
+if [ -z "$WORKING_DIR" ]; then
+    REPO=$(pwd)
+else
+    REPO=${WORKING_DIR}/apps
+fi
+ORG_NAME=${ORG_NAME:-operate-first}
+SOURCE_REPO=${SOURCE_REPO:-support}
+if [ -z "$ISSUE_NUMBER" ]; then
+    echo "ISSUE_NUMBER is not set"
+    exit 1
+fi
 
 # Unpack Config file, we will need these environment variables for the remainder of the steps
 PROJECT=$(yq e .project-name ${CONFIG})
@@ -25,11 +40,16 @@ QUOTA=$(yq e '.quota[0] | downcase' ${CONFIG})
 # Create OCP Group for team being onboarded
 
 GROUP_PATH="${REPO}/cluster-scope/base/user.openshift.io/groups/${GROUP}";
-mkdir ${GROUP_PATH}
-jsonnet --ext-str GROUP --ext-code USERS scripts/jsonnet/group.jsonnet | yq -P > ${GROUP_PATH}/group.yaml
-cd ${GROUP_PATH}
-kustomize init ${GROUP_PATH}
-kustomize edit add resource group.yaml
+# check if group already exists
+if [ -d "${GROUP_PATH}" ]; then
+    echo "Group ${GROUP} already exists, skipping creation"
+else
+  mkdir ${GROUP_PATH}
+  jsonnet --ext-str GROUP --ext-code USERS scripts/jsonnet/group.jsonnet | yq -P > ${GROUP_PATH}/group.yaml
+  cd ${GROUP_PATH}
+  kustomize init ${GROUP_PATH}
+  kustomize edit add resource group.yaml
+fi
 
 # Create namespace for team being onboarded
 
@@ -54,11 +74,16 @@ kustomize edit add component ../../../../components/resourcequotas/${QUOTA}
 
 cd ${REPO}
 RBAC_PATH="${REPO}/cluster-scope/components/project-admin-rolebindings/${GROUP}"
-mkdir ${RBAC_PATH}
-jsonnet --ext-str GROUP --ext-code USERS scripts/jsonnet/rbac.jsonnet | yq -P > ${RBAC_PATH}/rbac.yaml
-cd ${RBAC_PATH}
-kustomize init ${RBAC_PATH}
-kustomize edit add resource rbac.yaml
+# check if rolebinding already exists
+if [ -d "${RBAC_PATH}" ]; then
+    echo "Rolebinding ${GROUP}-${NAMESPACE} already exists, skipping creation"
+else
+  mkdir ${RBAC_PATH}
+  jsonnet --ext-str GROUP --ext-code USERS scripts/jsonnet/rbac.jsonnet | yq -P > ${RBAC_PATH}/rbac.yaml
+  cd ${RBAC_PATH}
+  kustomize init ${RBAC_PATH}
+  kustomize edit add resource rbac.yaml
+fi
 
 # Add rbac to the namespace being onboarded.
 
